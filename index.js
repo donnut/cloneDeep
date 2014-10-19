31edd7e755551b3f86cb64bf21f052aa6b6cb0e1
Erwin Poeze <erwin@mope.mobi>
2014-10-19 16:02:58 +0200

    adds proper cirref handling

--- index.js
+++ index.js
@@ -14,22 +14,18 @@
 }(this, function(R) {
 
     var toString = Object.prototype.toString;
-    var isArray = Array.isArray || function _isArray(val) {
-        return val && val.length >= 0 && toString.call(val) === '[object Array]';
-    };
-    var equalVal = function(val) {
-        return function(x) {
-            return x === val;
-        };
-    };
-
-    var refFrom = [];
 
     var cloneDeep = function(obj) {
+        var refFrom = [];
+        var refTo = [];
+        return baseClone(obj, refFrom, refTo);
+    }
+
+    var baseClone = function(obj, refFrom, refTo) {
         if (toString.call(obj) === '[object Object]') {
-            return copyObj(obj);
+            return copyObj(obj, refFrom, refTo);
         } else if (toString.call(obj) === '[object Array]') {
-            return copyArray(obj);
+            return copyArray(obj, refFrom, refTo);
         } else if (toString.call(obj) === '[object Function]') {
             return obj;
         } else if (toString.call(obj) === '[object Date]') {
@@ -39,34 +35,53 @@
         }
     };
 
-    var copyObj = function(obj) {
+    var copyObj = function(obj, refFrom, refTo) {
         var newObj = {};
         for (var key in obj) {
-            if (toString.call(obj[key]) === '[object Object]') {
-                var i = R.findIndex(equalVal(obj[key]), refFrom);
-                if (i !== -1) {
-                    newObj[key] = refFrom[i];
-                } else {
-                    refFrom.push(obj[key]);
-                    newObj[key] = cloneDeep(obj[key]);
-                }
-            } else {
-                newObj[key] = obj[key];
-            }
+            newObj[key] = copyObjValue(obj[key], refFrom, refTo);
         }
         return newObj;
     };
 
-    var copyArray = function(arr) {
+    var copyArray = function(arr, refFrom, refTo) {
         var newArr = [];
         for (var i = 0, len = arr.length; i < len; i++) {
-            if (R.is(Object, arr[i])) {
-                newArr.push(cloneDeep(arr[i]));
-            } else {
-                newArr.push(arr[i]);
-            }
+            newArr.push(copyObjValue(arr[i], refFrom, refTo));
         }
         return newArr;
+    };
+
+    var copyObjValue = function(value, refFrom, refTo) {
+        var copiedValue, i;
+        var duplicate = false;
+
+        if (toString.call(value) === '[object Object]' ||
+            toString.call(value) === '[object Array]') {
+            for (i = 0, len = refFrom.length; i < len; i++) {
+                if (value === refFrom[i]) {
+                    duplicate = true;
+                    break;
+                }
+            }
+
+            if (duplicate) {
+                copiedValue = refTo[i];
+            } else {
+                if (toString.call(value) === '[object Object]') {
+                    copiedValue = {};
+                } else if (toString.call(value) === '[object Array]') {
+                    copiedValue = [];
+                }
+                refFrom.push(value);
+                refTo.push(copiedValue);
+                for (var key in value) {
+                    copiedValue[key] = baseClone(value[key], refFrom, refTo);
+                }
+            }
+        } else {
+            copiedValue = value;
+        }
+        return copiedValue;
     };
 
     return cloneDeep;
